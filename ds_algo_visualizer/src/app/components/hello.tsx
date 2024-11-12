4762fadd2486f89c2c5073447533f243f1821046
Trevor Farquhar <trevorfarquhar2003@gmail.com>
2024-11-11 19:17:04 -0700

    can run application

---
+++ ds_algo_visualizer/src/app/components/hello.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import exp from "constants";
+
+function Hello() {
+    console.log("I am a client component");
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default Hello
