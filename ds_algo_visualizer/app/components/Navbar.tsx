a71beba2c4c4991ab40dcbc9b80b54c7c4c582ed
Trevor Farquhar <trevorfarquhar2003@gmail.com>
2024-11-18 22:52:44 -0700

    added test to navbar

--- ds_algo_visualizer/app/components/Navbar.tsx
+++ ds_algo_visualizer/app/components/Navbar.tsx
@@ -8,6 +8,9 @@ const Navbar = () => {
                 <Link href="/">
                     <Image src="/logo.png" alt="logo" width={144} height={30} />
                 </Link>
+
+                <h1 className="text-black">Test</h1> {/** This is a place holder for the other values in the navbar */}
+
             </nav>
         </header>
     )
