98fd1d0d76cb284075d2ec515ee4156af04bfa82
Trevor Farquhar <trevorfarquhar2003@gmail.com>
2024-11-20 21:48:53 -0700

    update to navebar

--- ds_algo_visualizer/app/components/Navbar.tsx
+++ ds_algo_visualizer/app/components/Navbar.tsx
@@ -1,19 +1,30 @@
+'use client'
 import Link from "next/link"
 import Image from "next/image"
-import { BrowserRouter, Route } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
 
 const Navbar = () => {
-    
+    // let navigate = useNavigate();
+    // function handleClick() {
+    //     navigate('/test_routes/test_routes')
+    // }
 
+    
     return (
         <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
                 <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={144} height={30} />
+                    {/* <Image src="/logo.png" alt="logo" width={144} height={30} /> */}
+                    <h1 className="text-3xl text-black">Site Name</h1>
                 </Link>
 
                 <div className="flex items-center gap-5 text-black">
-                    <button>Stacks</button>
+                    <ul>
+                        <li>
+                            <a href="/test"></a>
+                        </li>
+                    </ul>
                 </div>
 
             </nav>
